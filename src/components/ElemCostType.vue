be58ef8b81b1a5ae0627e6f8841bd9a2c90c3073
Alexey Sorokin <sorokin80@gmail.com>
2022-10-21 19:23:23 +0300

    costtype elem 1

---
+++ src/components/ElemCostType.vue
@@ -0,0 +1,198 @@
+<template>
+
+<!-- eslint-disable -->
+
+<div>
+
+<!-- Верхняя плашка -->
+   <Toolbar class="m-1 p-2 top-infobar">
+      <template #start>
+<!-- Путь    -->
+         <i class="fa fa-file-code text-primary text-3xl"/>
+         <span class="text-primary ml-2"> [{{costType.id}}] {{costType.path}}{{costType.name}}</span>
+      </template>
+   </Toolbar>
+
+
+<!-- Поля  -->
+   <Fieldset legend="Поля"  class="mt-2">
+<!--  Имя -->
+      <div class="field">
+         <label for="name" class="text-primary"> Имя </label>
+         <InputText id="name" type="username" aria-describedby="name-help" v-model="costType.name"
+                    :disabled="costType.readOnly" />
+      </div>
+<!--  Расходная/приходная    -->
+      <div class="field1">
+         <Checkbox inputId="isOutcome" v-model="costType.isOutcome" :binary="true" />
+         <label for="isOutcome" class="pl-2"> Расходная статья </label>
+      </div>
+<!--  Цвет    -->
+      <div class="field">
+      </div>
+   </Fieldset>
+
+
+<!-- Списки контроля доступа  -->
+   <Fieldset legend="Контроль доступа" class="mt-2 m-1">
+<!--  Владелец    -->
+      <div class="field">
+         <label for="owner" class="text-primary"> Владелец </label>
+         <Dropdown id="owner" v-model="costType.owner" :options="aclListOwner" optionValue="id" optionLabel="label" :filter="true" placeholder="список учетных записей..."
+                   :disabled="costType.readOnly"/>
+      </div>
+   </Fieldset>
+
+
+<!-- Нижняя панель инструментов -->
+   <Toolbar class="m-1 p-2">
+      <template #start>
+<!--  Флаг изменений        -->
+         <i class="fa fa-pen text-primary text-xl ml-2" v-if="dataChanged"/>
+      </template>
+      <template #end>
+<!--  Кнопки действий       -->
+         <Button label="Сохранить" icon="fa fa-save" class="mr-2 p-button-success" :disabled="costType.readOnly" @click="save()"/>
+         <Button label="Отмена" icon="fa fa-ban" class="mr-2 p-button-danger" @click="cancel()"/>
+      </template>
+   </Toolbar>
+
+</div>
+
+</template>
+
+
+<script>
+/* eslint-disable */
+
+import {apolloClient} from "@/apollo-config";
+import {authUtils} from "@/components/tools/auth-utils";
+import gql from "graphql-tag";
+import {clog} from "@/components/tools/vue-utils";
+
+export default {
+   name: "ElemCostType",
+
+   data() {
+      return {
+         costTypeId: Number(this.$route.params.id),
+         costType: {},
+         aclListOwner: null,
+         // --
+         dataChanged: false,
+      }
+   },
+
+   watch: {
+      // Признак внесения изменений в данные
+      costType: {
+         handler() {
+            this.dataChanged = true;
+         },
+         deep: true,
+      },
+      // --
+   },
+
+   mounted() {
+      // Запрос данных
+      const itemQ = gql(`
+            query($id: Int!) {
+               costtype(id: $id) {
+                  id, name, path, isOutcome, color,
+                  owner, acl, aclList,
+                  readOnly,
+               }
+            }
+      `);
+      apolloClient.query({
+         query: itemQ,
+         variables: {id: this.costTypeId},
+         fetchPolicy: "no-cache"} ).then( (response) => {
+            this.costType = response.data.costtype;
+            document.title =this.costType.name;
+            // -- owner
+            this.aclListOwner = JSON.parse(this.costType.aclList).slice(2);
+            // Костыль - нужно разобраться, какой компонент вызывает изменение данных при загрузке
+            setTimeout(() => { this.dataChanged = false }, 10);
+      }).catch( (error) => authUtils.err(error) );
+      // --
+   },
+
+   methods: {
+      // Кнопка Сохранить
+      async save() {
+         // -- Мутация - запись изменений
+         const updateM = gql(`
+               mutation ($id: Int!, $name: String!, $isOutcome: Boolean!, $color: String!, $owner: String!) {
+                  updateCosttype (id: $id, name: $name, isOutcome: $isOutcome, color: $color, owner: $owner) {
+                     ok, result
+                  }
+               }
+         `);
+         await apolloClient.mutate({
+            mutation: updateM,
+            variables: {
+               id: this.costType.id,
+               name: this.costType.name,
+               isOutcome: this.costType.isOutcome,
+               color: this.costType.color,
+               owner: this.costType.owner,
+            },
+            fetchPolicy: "no-cache"
+         }).then((response) => {
+            this.$toast.add({
+               severity: 'success',
+               summary: `Статья '${this.costType.name}'`,
+               detail: 'Успешно сохранена',
+               life: 2000
+            });
+         }).catch((error) => {
+            this.$toast.add({severity: 'error', summary: `Модуль AUTH`, detail: String(error)});
+            authUtils.err(error);
+         })
+         // this.$router.go(-1);
+      },
+      // Кнопка Отмена
+      cancel() {
+         this.$router.go(-1);
+      }
+   },
+
+}
+</script>
+
+
+<style>
+
+.p-fieldset-legend {
+   font-size: 0.9rem !important;
+   padding: 0.5rem !important;
+}
+
+</style>
+
+
+<style lang="scss" scoped>
+
+.sizes {
+   .p-inputtext {
+      display: block;
+      margin-bottom: .5rem;
+
+      &:last-child {
+         margin-bottom: 0;
+      }
+   }
+}
+
+.field .p-inputtext, {
+   display: block;
+}
+
+.field * {
+   width: 100%;
+   max-width: 50rem;
+}
+
+</style>
--- src/router.js
+++ src/router.js
@@ -3,10 +3,13 @@
 import {createRouter, createWebHistory } from 'vue-router';
 
 import CatProjects      from './components/CatProjects.vue';
-import CatAgents        from './components/CatAgents.vue';
-import CatCostTypes     from './components/CatCostTypes.vue';
 import ElemProject      from './components/ElemProject.vue';
+import CatCostTypes     from './components/CatCostTypes.vue';
+import ElemCostType     from './components/ElemCostType.vue';
+import CatAgents        from './components/CatAgents.vue';
+// import ElemAgent        from './components/ElemAgent.vue';
 // import LogFinOpers      from './components/LogFinOpers.vue';
+// import FinOper          from './components/FinOper.vue';
 import Settings from "@/components/Settings";
 
 export default createRouter({
@@ -15,8 +18,9 @@ export default createRouter({
         { path: '/cat-projects',    component: CatProjects, meta: { title: 'Проекты'}, alias: '/' },
         { path: '/project/:id',     component: ElemProject, meta: { title: 'Проект'} },
         { path: '/cat-costtypes',   component: CatCostTypes, meta: { title: 'Статьи'} },
-        // { path: '/costtype/:id',    component: ElemCostType, meta: { title: 'Статья'} },
+        { path: '/costtype/:id',    component: ElemCostType, meta: { title: 'Статья'} },
         { path: '/cat-agents',      component: CatAgents, meta: { title: 'Агенты'} },
+        // { path: '/agent/:id',       component: ElemAgent, meta: { title: 'Агент'} },
         // { path: '/log-finopers/',   component: LogFinOpers, meta: { title: 'Журнал операций'} },
         // { path: '/finoper/',        component: FinOper, meta: { title: 'Операция'} },
         { path: '/settings',        component: Settings, meta: { title: 'Настройки'} },
