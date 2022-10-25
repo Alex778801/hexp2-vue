edde0a45e8782cd716470b7aa129929422a454c5
Alexey Sorokin <sorokin80@gmail.com>
2022-10-25 21:41:52 +0300

    finoper carousel 1

--- src/components/FinOper.vue
+++ src/components/FinOper.vue
@@ -77,7 +77,17 @@
 
 <!-- ФОТО карусель -->
 
-
+   <Carousel :value="cars" :numVisible="1" :numScroll="1" :responsiveOptions="responsiveOptions">
+      <template #item="slotProps">
+         <div class="car-item">
+            <div class="car-content">
+               <div>
+                  <img :src="'demo/images/car/' + slotProps.data.image + '.png'" />
+               </div>
+            </div>
+         </div>
+      </template>
+   </Carousel>
 
 <!-- Списки контроля доступа  -->
    <Fieldset legend="Контроль доступа" class="mt-2 m-1">
@@ -120,11 +130,25 @@ export default {
 
    data() {
       return {
+
+         cars: [
+            {"id": "1000","code": "f230fh0g3","name": "Bamboo Watch","description": "Product Description","image": "bamboo-watch.jpg","price": 65,"category": "Accessories","quantity": 24,"inventoryStatus": "INSTOCK","rating": 5},
+            {"id": "1001","code": "nvklal433","name": "Black Watch","description": "Product Description","image": "black-watch.jpg","price": 72,"category": "Accessories","quantity": 61,"inventoryStatus": "INSTOCK","rating": 4},
+            {"id": "1002","code": "zz21cz3c1","name": "Blue Band","description": "Product Description","image": "blue-band.jpg","price": 79,"category": "Fitness","quantity": 2,"inventoryStatus": "LOWSTOCK","rating": 3},
+            {"id": "1003","code": "244wgerg2","name": "Blue T-Shirt","description": "Product Description","image": "blue-t-shirt.jpg","price": 29,"category": "Clothing","quantity": 25,"inventoryStatus": "INSTOCK","rating": 5},
+            {"id": "1004","code": "h456wer53","name": "Bracelet","description": "Product Description","image": "bracelet.jpg","price": 15,"category": "Accessories","quantity": 73,"inventoryStatus": "INSTOCK","rating": 4},
+         ],
+
+
+         // ИД операции
          operId: Number(this.$route.params.id),
+         // Фин операция
          oper: {'costType': {}, 'agentFrom': {}, 'agentTo': {}},
+         // Временное хранение МОМЕНТА
          ts: null,
+         // Список доступа ACL
          aclListUser: null,
-         // --
+         // Данные изменены пользователем
          dataChanged: false,
       }
    },
